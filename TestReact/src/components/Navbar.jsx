672860543e83e610819599c1b4753de8492ffd82
Federico Morgante <federicomorgante88@gmail.com>
2023-11-29 19:54:27 +0100

    test

---
+++ TestReact/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Wiki</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Contatti</a></li>
+            <li><a href="#">Logout</a></li>
+        </ul>
+    )
+}
+
+export default Navbar
